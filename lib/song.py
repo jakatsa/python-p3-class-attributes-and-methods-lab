55e7dc772e7e845f41d571d44a32ead0609173f5
Joe <jakatsa5@gmail.com>
2024-03-19 14:28:48 +0300

    completed

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,40 @@
 class Song:
-    pass
+    count = 0
+    genres = []
+    artists = []
+    genre_count = {}
+    artist_count = {}
+    all_songs = []
+
+    def __init__(self, name, artist, genre):
+        self.name = name
+        self.artist = artist
+        self.genre = genre
+        Song.add_song_to_count()
+        self.add_to_genres()
+        self.add_to_artists()
+        Song.all_songs.append(self)
+        Song.add_to_genre_count()
+        Song.add_to_artist_count()
+
+    @classmethod
+    def add_song_to_count(cls):
+        cls.count += 1
+
+    def add_to_genres(self):
+        if self.genre not in Song.genres:
+            Song.genres.append(self.genre)
+
+    def add_to_artists(self):
+        if self.artist not in Song.artists:
+            Song.artists.append(self.artist)
+
+    @classmethod
+    def add_to_genre_count(cls):
+        for genre in cls.genres:
+            cls.genre_count[genre] = sum(1 for song in cls.all_songs if song.genre == genre)
+
+    @classmethod
+    def add_to_artist_count(cls):
+        for artist in cls.artists:
+            cls.artist_count[artist] = sum(1 for song in cls.all_songs if song.artist == artist)
